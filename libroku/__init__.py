b1f295c5a7bd7015f6adff28b85b11d1a2622302
BBaoVanC <github@bbaovanc.com>
2020-09-06 17:54:13 -0500

    Add a couple more comments to libroku/__init__.py

--- libroku/__init__.py
+++ libroku/__init__.py
@@ -15,7 +15,7 @@ import urllib
 def sendbutton(pool, host, action, button, on_error):
     pressurl = "http://{0}:8060/{1}/{2}".format(host, action, button)  # The url to POST to
     pool.apply_async(requests.post, args=(pressurl,),
-                     error_callback=on_error)
+                     error_callback=on_error)  # asynchronously send the button request
     print("Sent: {0}/{1}".format(action, button))  # Inform the user of the request that has just been sent
 
 
@@ -23,6 +23,6 @@ def sendsearch(pool, host, rawquery, on_error):
     query = urllib.parse.quote(rawquery)  # Percent enccode any characters that need to be encoded
     searchurl = "http://{0}:8060/search/browse?keyword={1}".format(host, query)
     pool.apply_async(requests.post, args=(searchurl,),
-                     error_callback=on_error)
+                     error_callback=on_error)  # asynchronously send the search request
     print("Searching for: {0}".format(query))  # Inform the user of the request that has just been sent
 
